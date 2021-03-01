351b9d9a6b443ae93f05fc51388e68ec263c3515
poonamyadav12 <poonam273713@gmail.com>
2021-03-01 00:08:21 -0800

    Adding a few imports in the activity API file

--- Backend/apis/activity_api.js
+++ Backend/apis/activity_api.js
@@ -1,1 +1,5 @@
-// New activity file.
+import { connection, getConnection, getPool } from '../database/mysql.js';
+import { groupschema } from '../dataschema/group_schema.js';
+import { getTransactionsByGroupId } from './transactions_api.js';
+import { insertIfNotExist, getUserById } from './user_api.js';
+var Joi = require('joi');
