4053258d9d00adf6cafb7b58fe4b7a5b6e5d3291
poonamyadav12 <poonam273713@gmail.com>
2021-03-01 00:44:36 -0800

    Fixing a bug to not create connection again in Activity API

--- Backend/apis/activity_api.js
+++ Backend/apis/activity_api.js
@@ -14,19 +14,8 @@ export async function createActivity(conn, activity) {
 
     console.log(JSON.stringify(activity));
     const stmt = 'INSERT INTO Activities (Activity) VALUES (?)';
-    let conn;
-    try {
-        conn = await connection();
-        await conn.beginTransaction();
-        const modifiedActivity = JSON.parse(JSON.stringify(value.activity));
-        modifiedActivity.id = uuidv4();
-        await conn.query(stmt, [JSON.stringify(modifiedActivity)]);
-        await conn.commit();
-    } catch (err) {
-        await conn.rollback();
-        console.log(err);
-        throw err;
-    } finally {
-        conn && conn.release();
-    }
+    const modifiedActivity = JSON.parse(JSON.stringify(value.activity));
+    modifiedActivity.id = uuidv4();
+    await conn.query(stmt, [JSON.stringify(modifiedActivity)]);
+    await conn.commit();
 }
